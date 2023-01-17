a5655e8a95916be41f1df5357b05a44a5dd6915d
afeez1131 <lawalafeez052@gmail.com>
2023-01-17 09:48:15 +0100

    added counter to admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Phrase, ArticleTopic
+from .models import Phrase, ArticleTopic, Counter
 
 admin.site.register(Phrase)
+admin.site.register(Counter)
 admin.site.register(ArticleTopic)
